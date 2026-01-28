7f5e25ca8de3144e82540fa8bb643d0ae8fd5a61
BlaineOb <blnob567@gmail.com>
2026-01-28 15:39:44 -0500

    Eagerly load images in initial viewport

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   image: {
     layout: "constrained",
   },
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: "gruvbox-dark-hard",
